0ebcec86a04d6051503c899c375e782b56a59618
Mário Fernandes <mgustavofernandes@gmail.com>
2019-01-01 18:52:03 +0000

    updated transformations

--- transformtrack.py
+++ transformtrack.py
@@ -41,8 +41,25 @@ def scaleElems(tree,x,y):
     elems = getElements(tree)
     trackCollision,trackVisual,parkCollision,parkVisual,boundsCollision,boundsVisual,semaphoreCollision,semaphoreVisual = elems[:8]
     waypoints = elems[8:]
+    trackCollisionCoords = trackCollision.text.split(' ')
+    trackCollision.text = str(float(trackCollisionCoords[0])*x) + ' ' + str(float(trackCollisionCoords[1])*y)
+    trackVisual.text = str(x) + ' ' + str(y) + ' 1'
+    parkCollisionCoords = parkCollision.text.split(' ')
+    parkCollision.text = str(float(parkCollisionCoords[0])*x) + ' ' + str(float(parkCollisionCoords[1])*y)
+    parkVisualCoords = parkVisual.text.split(' ')
+    parkVisual.text = str(float(parkVisualCoords[0])*x) + ' ' + str(float(parkVisualCoords[1])*y)
+    boundsCollision.text = str(x) + ' ' + str(y) + ' 1'
+    boundsVisual.text = str(x) + ' ' + str(y) + ' 1'
+    semaphoreCollision.text = str(x) + ' ' + str(y) + ' 1'
+    semaphoreVisual.text = str(x) + ' ' + str(y) + ' 1'
+    it = iter(waypoints)
+    counter = 1
+    for waypoint in it:
+        waypoint.text = str(x) + ' ' + str(y) + ' 1'
+        next(it).text = str(x) + ' ' + str(y) + ' 1'
+    
 
 
 tree = ET.parse('src/conde_simulator/conde_world/worlds/conde_world.world')
-scaleElems(tree,1,1)
+scaleElems(tree,2,5)
 tree.write("transformed.world")
