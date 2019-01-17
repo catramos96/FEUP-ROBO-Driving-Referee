9a6f13d4d6341da03d1761f94a5d4e6be09349b3
Mário Fernandes <mgustavofernandes@gmail.com>
2019-01-17 20:11:15 +0000

    update script

--- transformtrack.py
+++ transformtrack.py
@@ -6,53 +6,35 @@ def getElements(tree):
     elems = list()
     elems.append(root.find('./world[@name="default"]/model[1]/link[@name="link_ground"]/collision[@name="collision"]/geometry/plane/size'))
     elems.append(root.find('./world[@name="default"]/model[1]/link[@name="link_ground"]/visual[@name="visual"]/geometry/mesh/scale'))
-    elems.append(root.find('./world[@name="default"]/model[2]/link[@name="link_park"]/collision[@name="park"]/geometry/plane/size'))
-    elems.append(root.find('./world[@name="default"]/model[2]/link[@name="link_park"]/visual[@name="visual_park"]/geometry/plane/size'))
-    elems.append(root.find('./world[@name="default"]/model[3]/link[@name="bumper_sensor"]/collision[@name="bumper_sensor"]/geometry/mesh/scale'))
-    elems.append(root.find('./world[@name="default"]/model[3]/link[@name="bumper_sensor"]/visual[@name="visual"]/geometry/mesh/scale'))
-    elems.append(root.find('./world[@name="default"]/model[4]/link[@name="bumper_sensor"]/collision[@name="bumper_sensor"]/geometry/mesh/scale'))
-    elems.append(root.find('./world[@name="default"]/model[4]/link[@name="bumper_sensor"]/visual[@name="visual"]/geometry/mesh/scale'))
-    for i in range(1,9):
-        elems.append(root.find('./world[@name="default"]/model['+str(i+4)+']/link[@name="bumper_sensor"]/collision[@name="bumper_sensor"]/geometry/mesh/scale'))
-        elems.append(root.find('./world[@name="default"]/model['+str(i+4)+']/link[@name="bumper_sensor"]/visual[@name="visual"]/geometry/mesh/scale'))
+    elems.append(root.find('./world[@name="default"]/model[2]/link[@name="link_park"]/collision[@name="park"]/geometry/mesh/scale'))
+    elems.append(root.find('./world[@name="default"]/model[2]/link[@name="link_park"]/visual[@name="visual_park"]/geometry/mesh/scale'))
+    for i in range(1,16):
+        elems.append(root.find('./world[@name="default"]/model['+str(i+2)+']/link[@name="bumper_sensor"]/collision[@name="bumper_sensor"]/geometry/mesh/scale'))
+        elems.append(root.find('./world[@name="default"]/model['+str(i+2)+']/link[@name="bumper_sensor"]/visual[@name="visual"]/geometry/mesh/scale'))
     return elems
 
 
 def printElements(tree):
     elems = getElements(tree)
-    trackCollision,trackVisual,parkCollision,parkVisual,boundsCollision,boundsVisual,semaphoreCollision,semaphoreVisual = elems[:8]
-    waypoints = elems[8:]
+    trackCollision,trackVisual = elems[:2]
+    sensors = elems[2:]
     print('Track Floor Collision: ' + trackCollision.text)
     print('Track Floor Visual Scaling: ' + trackVisual.text)
-    print('Park Floor Collision: ' + parkCollision.text)
-    print('Park Floor Visual: ' + parkVisual.text)
-    print('Track Bounds Collision Scaling: ' + boundsCollision.text)
-    print('Track Bounds Visual: ' + boundsVisual.text)
-    print('Semaphore Collision Scaling: ' + semaphoreCollision.text)
-    print('Semaphore Visual Scaling: ' + semaphoreVisual.text)
-    it = iter(waypoints)
+    it = iter(sensors)
     counter = 1
     for waypoint in it:
-        print('Waypoint '+str(counter)+' Collision Scaling: ' + waypoint.text)
-        print('Waypoint '+str(counter)+' Visual Scaling: ' + next(it).text)
+        print('Sensor '+str(counter)+' Collision Scaling: ' + waypoint.text)
+        print('Sensor '+str(counter)+' Visual Scaling: ' + next(it).text)
         counter = counter + 1
 
 def scaleElems(tree,x,y):
     elems = getElements(tree)
-    trackCollision,trackVisual,parkCollision,parkVisual,boundsCollision,boundsVisual,semaphoreCollision,semaphoreVisual = elems[:8]
-    waypoints = elems[8:]
+    trackCollision,trackVisual = elems[:2]
+    sensors = elems[2:]
     trackCollisionCoords = trackCollision.text.split(' ')
     trackCollision.text = str(float(trackCollisionCoords[0])*x) + ' ' + str(float(trackCollisionCoords[1])*y)
     trackVisual.text = str(x) + ' ' + str(y) + ' 1'
-    parkCollisionCoords = parkCollision.text.split(' ')
-    parkCollision.text = str(float(parkCollisionCoords[0])*x) + ' ' + str(float(parkCollisionCoords[1])*y)
-    parkVisualCoords = parkVisual.text.split(' ')
-    parkVisual.text = str(float(parkVisualCoords[0])*x) + ' ' + str(float(parkVisualCoords[1])*y)
-    boundsCollision.text = str(x) + ' ' + str(y) + ' 1'
-    boundsVisual.text = str(x) + ' ' + str(y) + ' 1'
-    semaphoreCollision.text = str(x) + ' ' + str(y) + ' 1'
-    semaphoreVisual.text = str(x) + ' ' + str(y) + ' 1'
-    it = iter(waypoints)
+    it = iter(sensors)
     counter = 1
     for waypoint in it:
         waypoint.text = str(x) + ' ' + str(y) + ' 1'
